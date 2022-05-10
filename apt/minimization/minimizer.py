More tests + fix error in NCP calculation

apt/minimization/minimizer.py
@@ -173,7 +173,6 @@ class GeneralizeToRepresentative(BaseEstimator, MetaEstimatorMixin, TransformerM
 
         if dataset and dataset.get_samples() is not None and dataset.get_labels() is not None:
             self.n_features_ = dataset.get_samples().shape[1]
-
         elif dataset and dataset.features_names:
             self.n_features_ = len(dataset.features_names)
         else:
@@ -231,7 +230,7 @@ class GeneralizeToRepresentative(BaseEstimator, MetaEstimatorMixin, TransformerM
                         fd['max'] = max(values)
                         fd['range'] = max(values) - min(values)
                     else:
-                        fd['range'] = len(values)
+                        fd['range'] = len(np.unique(values))
                     feature_data[feature] = fd
 
             # prepare data for DT

tests/test_anonymizer.py
@@ -64,6 +64,7 @@ def test_anonymize_pandas_adult():
     assert (anon.loc[:, QI].value_counts().min() >= k)
     np.testing.assert_array_equal(anon.drop(QI, axis=1), x_train.drop(QI, axis=1))
 
+
 def test_anonymize_pandas_nursery():
     (x_train, y_train), _ = get_nursery_dataset_pd()
     x_train = x_train.astype(str)
@@ -98,7 +99,6 @@ def test_anonymize_pandas_nursery():
 
 
 def test_regression():
-
     dataset = load_diabetes()
     x_train, x_test, y_train, y_test = train_test_split(dataset.data, dataset.target, test_size=0.5, random_state=14)
 