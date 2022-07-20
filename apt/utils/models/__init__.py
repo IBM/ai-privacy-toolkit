9621d973847ea482516c9cd90606de4aa8eae6a4
abigailt <abigailt@il.ibm.com>
2022-07-20 14:17:25 +0300

    Keras regression support

--- apt/utils/models/__init__.py
+++ apt/utils/models/__init__.py
@@ -2,4 +2,4 @@ from apt.utils.models.model import Model, BlackboxClassifier, ModelOutputType, S
     BlackboxClassifierPredictions, BlackboxClassifierPredictFunction, get_nb_classes, is_one_hot, \
     check_correct_model_output
 from apt.utils.models.sklearn_model import SklearnModel, SklearnClassifier, SklearnRegressor
-from apt.utils.models.keras_model import KerasClassifier
+from apt.utils.models.keras_model import KerasClassifier, KerasRegressor
