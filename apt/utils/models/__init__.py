b433eff010673fc51756fd57fb78437613eb7e6a
abigailt <abigailt@il.ibm.com>
2022-07-28 16:31:08 +0300

    Add support for xgboost XGBClassifier (#53)

--- apt/utils/models/__init__.py
+++ apt/utils/models/__init__.py
@@ -3,3 +3,4 @@ from apt.utils.models.model import Model, BlackboxClassifier, ModelOutputType, S
     check_correct_model_output
 from apt.utils.models.sklearn_model import SklearnModel, SklearnClassifier, SklearnRegressor
 from apt.utils.models.keras_model import KerasClassifier, KerasRegressor
+from apt.utils.models.xgboost_model import XGBoostClassifier
