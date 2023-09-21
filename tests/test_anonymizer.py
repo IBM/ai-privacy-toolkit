a814404534bf9c6377eacc595971713fdbf86658
abigailt <abigailt@il.ibm.com>
2023-09-21 19:00:27 +0300

    Support 1-hot encoded features in anonymization (#72)
    Signed-off-by: abigailt <abigailt@il.ibm.com>

--- tests/test_anonymizer.py
+++ tests/test_anonymizer.py
@@ -118,6 +118,35 @@ def test_regression():
     assert ((np.delete(anon, QI, axis=1) == np.delete(x_train, QI, axis=1)).all())
 
 
+def test_anonymize_ndarray_one_hot():
+    x_train = np.array([[23, 0, 1, 165],
+                          [45, 0, 1, 158],
+                          [56, 1, 0, 123],
+                          [67, 0, 1, 154],
+                          [45, 1, 0, 149],
+                          [42, 1, 0, 166],
+                          [73, 0, 1, 172],
+                          [94, 0, 1, 168],
+                          [69, 0, 1, 175],
+                          [24, 1, 0, 181],
+                          [18, 1, 0, 190]])
+    y_train = np.array([1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0])
+
+    model = DecisionTreeClassifier()
+    model.fit(x_train, y_train)
+    pred = model.predict(x_train)
+
+    k = 10
+    QI = [0, 1, 2]
+    QI_slices = [[1, 2]]
+    anonymizer = Anonymize(k, QI, train_only_QI=True, quasi_identifer_slices=QI_slices)
+    anon = anonymizer.anonymize(ArrayDataset(x_train, pred))
+    assert (len(np.unique(anon[:, QI], axis=0)) < len(np.unique(x_train[:, QI], axis=0)))
+    _, counts_elements = np.unique(anon[:, QI], return_counts=True)
+    assert (np.min(counts_elements) >= k)
+    assert ((np.delete(anon, QI, axis=1) == np.delete(x_train, QI, axis=1)).all())
+
+
 def test_errors():
     with pytest.raises(ValueError):
         Anonymize(1, [0, 2])
